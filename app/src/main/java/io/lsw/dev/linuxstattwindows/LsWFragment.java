4261480f3b1d922ef179729dbb29c12cb761a24c
Neotrace <DanielJankowski@rub.de>
2015-05-20 10:59:54 +0200

    Fixed JavaScript, should remove navbar on every device(thanks to rbeer)

--- app/src/main/java/io/lsw/dev/linuxstattwindows/LsWFragment.java
+++ app/src/main/java/io/lsw/dev/linuxstattwindows/LsWFragment.java
@@ -1,14 +1,10 @@
 package io.lsw.dev.linuxstattwindows;
 
 import android.app.Fragment;
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -27,7 +23,7 @@ public class LsWFragment extends Fragment {
         }
         @Override
         public void onPageFinished(WebView view, String url) {
-            view.loadUrl("javascript: function reformat_mobile() { document.getElementById(\"header-menu\").remove(); document.querySelector(\"body\").style.paddingTop = \"20px\"; } reformat_mobile(); ");
+            view.loadUrl("javascript: function reformat_mobile() { document.getElementById(\"header-menu\").parentNode.removeChild(document.getElementById(\"header-menu\")); document.querySelector(\"body\").style.paddingTop = \"20px\"; } reformat_mobile(); ");
         }
     }
 
